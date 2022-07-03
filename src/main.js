d1f25c69ef2634e7fc1cced709f86d8532c39044
shlee <completed0728@gmail.com>
2022-07-03 10:20:47 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-
 import VueCordova from 'vue-cordova';
+import CORDOVA_API from '@/utils/cordova.js';
 Vue.use(VueCordova);
-
+Vue.prototype.$CORDOVA_API = CORDOVA_API;
 Vue.config.productionTip = false;
 
 new Vue({
